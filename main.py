ecc372ee3d8fa7a151e2ec5f1c1393f211e0b6aa
최재용 <jayong93@gmail.com>
2018-10-11 12:14:31 +0900

    수정

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 
 user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1'
 
+@app.route('/')
+def home():
+    return "hello"
+
 @app.route('/play_audio/')
 def play_audio():
     url = request.args.get('src')
