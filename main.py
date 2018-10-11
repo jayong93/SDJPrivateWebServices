d97c6de4ec49636fd4ec19dcb97767f03b140b55
최재용 <jayong93@gmail.com>
2018-10-11 23:16:39 +0900

    html에 스크립트 추가해서 자동 실행

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template, request
 import requests as req
+import urllib
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1'
 
-@app.route('/play_audio/')
+@app.route('/play_podbbang/')
 def play_audio():
-    url = request.args.get('src')
-    html = req.get(url, headers={'User-Agent': user_agent}).text
-    bs = BeautifulSoup(html, 'html.parser')
-    file_url = bs.find(id="audiofile")['value']
-    return render_template('play_audio.html', src=file_url, time_offset=request.args.get('offset'))
-    #return page
-    #return render_template('play_audio.html', src=, time_offset=request.args.get('offset'))
+    url = request.args.get('url')
+    new_header = {key:val for key,val in request.headers if key != "Host"}
+    response = req.get(url, headers=new_header)
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    new_script = soup.new_tag("script")
+    new_script.string = '$(window).on("load", function(){$("#play").click();});'
+    soup.head.append(new_script)
+    print(soup.head)
+
+    return str(soup)
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
